21e4f0ffd76bab9d9825b1320ddc371e796200f9
Piyush Sinha <piyushsinha@Piyushs-MacBook-Air.local>
2020-02-20 13:45:44 +0530

    debounce search

--- lib/bloc/player_listing_bloc.dart
+++ lib/bloc/player_listing_bloc.dart
@@ -5,10 +5,21 @@ import 'package:flutter_bloc_example/bloc/player_listing_events.dart';
 import 'package:flutter_bloc_example/bloc/player_listing_states.dart';
 import 'package:flutter_bloc_example/models/api_models.dart';
 import 'package:flutter_bloc_example/services/repository.dart';
+import 'package:rxdart/rxdart.dart';
 
 class PlayerListingBloc extends Bloc<PlayerListingEvent, PlayerListingState> {
   final PlayerRepository playerRepository;
   PlayerListingBloc({this.playerRepository}) : assert(playerRepository != null);
+
+  @override Stream<PlayerListingState> transformEvents(Stream<PlayerListingEvent> events, Stream<PlayerListingState> Function(PlayerListingEvent event) next) {
+    // TODO: implement transformEvents
+    return super.transformEvents(
+      (events as Observable<PlayerListingEvent>).debounce(
+        (_) => TimerStream(true, const Duration(milliseconds: 250)))
+      ,next,
+    );
+  }
+
    @override
   void onTransition(Transition<PlayerListingEvent, PlayerListingState> transition) {
     super.onTransition(transition);
@@ -26,6 +37,7 @@ class PlayerListingBloc extends Bloc<PlayerListingEvent, PlayerListingState> {
           players = await playerRepository
               .fetchPlayersByCountry(event.nationModel.countryId);
         } else if (event is SearchTextChangedEvent) {
+          print("Hitting service");
           players = await playerRepository.fetchPlayersByName(event.searchTerm);
         }
         if (players.length == 0) {
